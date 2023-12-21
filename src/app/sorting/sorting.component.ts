62bd968de732c1efe78ba3704d6e626bb8862c74
manuelarte <manuel.doncel.martos@gmail.com>
2023-12-21 15:24:51 +0100

    sorting finished

--- src/app/sorting/sorting.component.ts
+++ src/app/sorting/sorting.component.ts
@@ -10,17 +10,18 @@ import {CommonModule} from "@angular/common";
 import {TarkovApiService} from "../../tarkov-api.service";
 import {Ammo} from "../ammo.model";
 import {AmmoListComponent} from "../ammo-list/ammo-list.component";
-import {finalize} from "rxjs";
+import {finalize, Observable} from "rxjs";
 import {MatProgressSpinnerModule} from "@angular/material/progress-spinner";
 import {
   CdkDrag,
   CdkDragDrop,
   CdkDragPlaceholder,
   CdkDropList,
-  moveItemInArray,
-  transferArrayItem
+  moveItemInArray
 } from "@angular/cdk/drag-drop";
 import {MatGridListModule} from "@angular/material/grid-list";
+import {MatIconModule} from "@angular/material/icon";
+import {MatButtonModule} from "@angular/material/button";
 
 class SortingBy {
   name: string
@@ -30,12 +31,46 @@ class SortingBy {
     this.name = name
     this.f = f
   }
+
+  sort(ammos: Ammo[]) {
+    ammos.sort((a, b) => {
+      if (this.f(a) > this.f(b)) {
+        return -1
+      } else if (this.f(a) < this.f(b)) {
+        return 1
+      } else {
+        return 0
+      }
+    })
+  }
 }
+
+class SortingGame {
+  solution: Ammo[]
+  sortingBy: SortingBy
+
+  constructor(ammos: Ammo[], sortingBy: SortingBy) {
+    this.sortingBy = sortingBy
+    this.solution = [...ammos]
+    this.sortingBy.sort(this.solution)
+  }
+
+  isCompleted(ammos: Ammo[]): boolean {
+    if (ammos.length == this.solution.length) {
+      return ammos.every((e, index) => this.sortingBy.f(e) === this.sortingBy.f(this.solution[index]))
+    }
+    return false
+  }
+}
+
+const DAMAGE = new SortingBy("damage", (a: Ammo) => a.damage)
+const PENETRATION_POWER = new SortingBy("penetration power", (a: Ammo) => a.penetrationPower)
+const SORTING_OPTIONS = [DAMAGE, PENETRATION_POWER]
 
 @Component({
   selector: 'app-sorting',
   standalone: true,
-  imports: [CommonModule, RouterOutlet, RouterLink, RouterLinkActive, AmmoListComponent, MatProgressSpinnerModule, CdkDropList, CdkDrag, CdkDragPlaceholder, MatGridListModule],
+  imports: [CommonModule, RouterOutlet, RouterLink, RouterLinkActive, AmmoListComponent, MatProgressSpinnerModule, CdkDropList, CdkDrag, CdkDragPlaceholder, MatGridListModule, MatIconModule, MatButtonModule],
   templateUrl: './sorting.component.html',
   styleUrl: './sorting.component.css'
 })
@@ -45,14 +80,14 @@ export class SortingComponent implements OnInit {
   protected seed: number = 0
 
   protected isLoadingAmmos = true
+  protected allAmmos: Ammo[] = []
   protected ammos: Ammo[] = []
 
-  protected solution: Ammo[] = []
+  protected sortingGame: SortingGame = new SortingGame([], DAMAGE)
   protected numberOfMoves = 0
   protected isCompleted = false
 
-  protected options = [new SortingBy("damage", (a: Ammo) => a.damage), new SortingBy("penetration power", (a: Ammo) => a.penetrationPower)]
-  protected sortedOption: SortingBy = this.options[0];
+  protected sortedOption: SortingBy = SORTING_OPTIONS[0];
   constructor(
     private route: ActivatedRoute,
     private router: Router,
@@ -64,7 +99,7 @@ export class SortingComponent implements OnInit {
     this.route.queryParams.subscribe(params => {
       if (params['seed']) {
         this.seed = +params['seed']; // Convert to number
-        this.sortedOption = this.options[Math.floor(Math.random() * this.options.length)];
+        this.sortedOption = this.getRandomNElements(SORTING_OPTIONS, 1)[0];
       } else {
         this.seed = Math.floor(Math.random() * 1000);
 
@@ -75,48 +110,36 @@ export class SortingComponent implements OnInit {
           queryParamsHandling: 'merge'
         });
       }
-    });
-
-    this.isLoadingAmmos = true
-    this.api.getAmmo().pipe(
-      finalize(() => this.isLoadingAmmos = false)
-    ).subscribe(ammos => {
-      this.ammos = this.getRandomAmmos(ammos)
-      this.solution = [...this.ammos]
-      this.solution.sort((a, b) => {
-        if (this.sortedOption.f(a) > this.sortedOption.f(b)) {
-          return -1
-        } else if (this.sortedOption.f(a) < this.sortedOption.f(b)) {
-          return 1
-        } else {
-          return 0
-        }
+      this.loadAmmos().subscribe(allAmmos => {
+        this.allAmmos = allAmmos
+        this.createGame()
       })
-      console.log(this.solution)
-    })
+
+    });
+  }
+
+  private loadAmmos(): Observable<Ammo[]> {
+    this.isLoadingAmmos = true
+    return this.api.getAmmo().pipe(
+      finalize(() => this.isLoadingAmmos = false)
+    )
+  }
+
+  private createGame() {
+    this.ammos = this.getRandomAmmos(this.allAmmos)
+    this.sortingGame = new SortingGame(this.ammos, this.sortedOption)
+  }
+
+  private getRandomNElements<T>(array: T[], n: number): T[] {
+    const copy = [...array]
+    return this.shuffle(copy, this.seed).slice(0, n)
   }
 
   private getRandomAmmos(ammos: Ammo[]): Ammo[] {
-    return this.shuffle(ammos, this.seed).slice(0, this.numberOfItems)
+    return this.getRandomNElements(ammos, this.numberOfItems)
   }
 
-  dropAmmo(event: CdkDragDrop<Ammo[]>) {
-    moveItemInArray(event.container.data, event.previousIndex, event.currentIndex)
-    if (event.currentIndex != event.previousIndex) {
-      this.numberOfMoves++
-      this.isCompleted = this.checkSolution()
-    }
-  }
-
-  private checkSolution(): boolean {
-    // TODO compare by...
-    if (this.ammos.length == this.solution.length) {
-      return this.ammos.every((e, index) => this.sortedOption.f(e) === this.sortedOption.f(this.solution[index]))
-    }
-    return false
-  }
-
-  private shuffle(array: Ammo[], seed: number): Ammo[] {
+  private shuffle<T>(array: T[], seed: number): T[] {
     let currentIndex = array.length, temporaryValue, randomIndex;
     seed = seed || 1;
     let random = function() {
@@ -136,4 +159,20 @@ export class SortingComponent implements OnInit {
     return array;
   }
 
+  dropAmmo(event: CdkDragDrop<Ammo[]>) {
+    moveItemInArray(event.container.data, event.previousIndex, event.currentIndex)
+    if (event.currentIndex != event.previousIndex) {
+      this.numberOfMoves++
+      this.isCompleted = this.sortingGame.isCompleted(this.ammos)
+    }
+  }
+
+  newGame() {
+    this.router.navigateByUrl('/sorting',{skipLocationChange:true}).then(()=>{
+      this.router.navigate([], {
+        relativeTo: this.route,
+        queryParamsHandling: 'merge'
+      });
+    })
+  }
 }
